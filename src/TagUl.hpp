1d2da0e9baf8b1986607ccb154ff7f7e03e92685
AssirDaniilov <stm8725721@mail.ru>
2016-12-24 01:19:36 +0300

    Add files via upload

---
+++ src/TagUl.hpp
@@ -0,0 +1,18 @@
+#ifndef TAGUL
+#define TAGUL
+
+#include <parser.h>
+#include <QWidget>
+#include "render.hpp"
+
+namespace Render
+{
+     class TagUl: public QWidget
+     {
+         Q_OBJECT
+     public:
+         TagUl(const Parser::Tree::Tag &parsTag,QWidget* parent);
+     };
+}
+
+#endif
