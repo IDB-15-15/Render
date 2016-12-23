dd23b23d61422409e2c775b53172f71638b92f06
Nekit228 <panatikin@gmail.com>
2016-12-24 01:50:21 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
+
 #include "modulsystem.hpp"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
-    std::shared_ptr<std::string> input = std::make_shared<std::string>("<html><head><title>KEKEKEKEEK</title></head><body text='aqua' font-size='5'>blabla<a href='ya.ru'>yandex</a>test</img></body></html>");
+    std::shared_ptr<std::string> input = std::make_shared<std::string>
+        ("<html><head><title>KEKEKEKEEK</title></head><body text='aqua' font-size='5'>blabla<a href='ya.ru'>yandex</a>test</img></body></html>");
     Parser::Tree tree = Parser::parse(input);
     Parser::Tree::Tag root = boost::get<Parser::Tree::Tag>(tree.root);
     QApplication app(argc, argv);
