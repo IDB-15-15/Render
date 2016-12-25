7df340e183ef766518f616704b4dcf0838c42686
AssirDaniilov <stm8725721@mail.ru>
2016-12-25 22:45:19 +0300

    Update TagDl.hpp

--- src/TagDl.hpp
+++ src/TagDl.hpp
@@ -4,8 +4,26 @@
 #include <parser.h>
 #include <QWidget>
 #include "render.hpp"
+#include <QtWidgets/QMainWindow>
 
-namespace Render{
+namespace Ui
+{
+    class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+    public:
+       explicit MainWindow(QWidget *parent = 0);
+   
+       ~MainWindow();
+    private:
+    Ui::MainWindow *ui;
+};    
+    
+namespace Render
+{
 
 class TagDl: public QWidget
 {
