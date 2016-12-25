03f8ac5f429e7b0361550d2be1347e9213e5db4f
AssirDaniilov <stm8725721@mail.ru>
2016-12-25 23:14:08 +0300

    Update TagUl.hpp

--- src/TagUl.hpp
+++ src/TagUl.hpp
@@ -4,23 +4,7 @@
 #include <parser.h>
 #include <QWidget>
 #include "render.hpp"
-#include <QtWidgets/QMainWindow>
 
-namespace Ui
-{
-    class MainWindow;
-}
-
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
-    public:
-       explicit MainWindow(QWidget *parent = 0);
-   
-       ~MainWindow();
-    private:
-    Ui::MainWindow *ui;
-};    
 
 namespace Render
 {
